b6d2fd78eb2bbc389c6607dee1d46b217c4ed01a
mmsss <mmsss875@gmail.com>
2022-09-18 13:43:27 +0300

    MMT

--- resources/views/layout/_footer.blade.php
+++ resources/views/layout/_footer.blade.php
@@ -24,10 +24,10 @@
                 <div class="cv-foot-box cv-foot-links">
                     <h2>@lang("sts.Quick links")</h2>
                     <ul>
-                        <li><a href="javascript">@lang("sts.Services")</a></li>
-                        <li><a href="javascript">@lang("sts.Product Gallery")</a></li>
-                        <li><a href="javascript">@lang("sts.About Us")</a></li>
-                        <li><a href="javascript">@lang("sts.Contact Us")</a></li>
+                        <li><a href="{{url("/#service")}}">@lang("sts.Services")</a></li>
+                        <li><a href="{{url("/#gallery")}}">@lang("sts.Product Gallery")</a></li>
+                        <li><a href="{{url("/#about_us")}}">@lang("sts.About Us")</a></li>
+                        <li><a href="{{url("/#contact_us")}}">@lang("sts.Contact Us")</a></li>
                         {{--<li><a href="javascript">@lang("sts.Privacy Policy")</a></li>--}}
                     </ul>
                 </div>
@@ -37,11 +37,13 @@
                     <h2>@lang("sts.Follow Us")</h2>
                     <div class="cv-foot-box cv-foot-contact">
                         <p><a target="_blank" href="{{@$follow_us->facebook}}" class="nav-link p-0"><i
-                                    style="color:#3cbcff;"   class="fab fa-facebook-f fa-fw"></i> @lang("sts.Facebook") </a></p>
+                                    style="color:#3cbcff;" class="fab fa-facebook-f fa-fw"></i> @lang("sts.Facebook")
+                            </a></p>
                         <p><a target="_blank" href="{{@$follow_us->instagram}}" class="nav-link p-0"><i
-                                    style="color:#3cbcff;"   class="fab fa-instagram fa-fw"></i> @lang("sts.Instagram") </a></p>
+                                    style="color:#3cbcff;" class="fab fa-instagram fa-fw"></i> @lang("sts.Instagram")
+                            </a></p>
                         <p><a target="_blank" href="{{@$follow_us->twitter}}" class="nav-link p-0"><i
-                                    style="color:#3cbcff;"  class="fab fa-twitter fa-fw"></i> @lang("sts.Twitter")
+                                    style="color:#3cbcff;" class="fab fa-twitter fa-fw"></i> @lang("sts.Twitter")
                             </a></p>
                     </div>
                     {{--<ul class="list-unstyled">
@@ -58,11 +60,13 @@
             <div class="col-lg-4 col-md-6">
                 <div class="cv-foot-box cv-foot-contact">
                     <h2>@lang("sts.Contact")</h2>
-                    <p><a href="tel:+55 456-789-9651"><i class="fa fa-map-marker-alt fa-fw" style="color:#3cbcff;"></i> {{@$follow_us->mobile}}</a></p>
+                    <p><a href="tel:+55 456-789-9651"><i class="fa fa-map-marker-alt fa-fw"
+                                                         style="color:#3cbcff;"></i> {{@$follow_us->mobile}}</a></p>
                     <p><a href="mailto:support@domian.com"><i class="fa fa-phone-alt fa-fw" style="color:#3cbcff;"></i>
-                        {{@$follow_us->email}}</a></p>
+                            {{@$follow_us->email}}</a></p>
                     <p><a href="http://maps.google.com/?q=785 LNCT block street, USA"><i class="fa fa-envelope fa-fw"
-                                                                                         style="color:#3cbcff;"></i> {{@custom_lang(@$follow_us,"location")}}</a></p>
+                                                                                         style="color:#3cbcff;"></i> {{@custom_lang(@$follow_us,"location")}}
+                        </a></p>
                 </div>
             </div>
         </div>
@@ -75,7 +79,7 @@
         <div class="row">
             <div class="col-md-12">
                 <p>&copy; @lang("sts.copy_right")<a target="_blank" class="link-light fw-bolder"
-                                                    href="https://moomena.com/">@lang("sts.dev_by")</a></p>
+                                                    href="https://moomena.com">@lang("sts.dev_by")</a></p>
             </div>
         </div>
     </div>
